d8ad78be46fb05059acbc2c37424963601b07059
Raphael Sizemore <rmorgansizemore@gmail.com>
2023-03-24 02:32:23 +0000

    initial commit for odin

---
+++ designs/odin/src/index.mjs
@@ -0,0 +1,15 @@
+//
+
+import { Design } from '@freesewing/core'
+import { data } from '../data.mjs'
+// Parts
+import { box } from './box.mjs'
+
+// Create new design
+const Odin = new Design({
+  data,
+  parts: [box],
+})
+
+// Named exports
+export { box, Odin }
